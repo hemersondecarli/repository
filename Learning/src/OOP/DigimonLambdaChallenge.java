aab57adc4a8ac188fd9df3d5febfa4f5ec2d71be
Hemerson <hemersondc96@gmail.com>
2026-05-16 23:42:57 +0100

    created a class digimon  and a Digimon object in an arrayList

--- Learning/src/OOP/DigimonLambdaChallenge.java
+++ Learning/src/OOP/DigimonLambdaChallenge.java
@@ -1,9 +1,61 @@
 package OOP;
 
+import java.util.ArrayList;
+
 public class DigimonLambdaChallenge {
 
+	class Digimon {
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getLevel() {
+			return level;
+		}
+
+		public void setLevel(int level) {
+			this.level = level;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public int getHealth() {
+			return health;
+		}
+
+		public void setHealth(int health) {
+			this.health = health;
+		}
+
+		public void showDigimon() {
+			System.out.println("Name: " + name + " level: " + level + " type: " + type + " health: " + health);
+		}
+
+		private String name;
+		private int level;
+		private String type;
+		private int health;
+
+		public Digimon(String name, int level, String type, int health) {
+			this.name = name;
+			this.level = level;
+			this.type = type;
+			this.health = health;
+		}
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<Digimon> digimons = new ArrayList<>();
 
 	}
 
