66c47deb8acf62b5ebbdf54257a5ea62bfd881c3
Hemerson <hemersondc96@gmail.com>
2026-05-26 23:45:46 +0100

    Stream Methods

---
+++ Learning/src/Stream/StreamMethodSystem.java
@@ -0,0 +1,12 @@
+package Stream;
+
+import java.util.ArrayList;
+
+public class StreamMethodSystem {
+
+	public static void main(String[] args) {
+		ArrayList<Pokemon> pokemons = new ArrayList<>();
+
+	}
+
+}
