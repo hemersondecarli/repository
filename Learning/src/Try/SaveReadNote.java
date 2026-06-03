a7a2b70ff33d737b5e2413034d297ef41e35b6b9
Hemerson <hemersondc96@gmail.com>
2026-06-04 00:06:56 +0100

    File writer file reader

--- Learning/src/Try/SaveReadNote.java
+++ Learning/src/Try/SaveReadNote.java
@@ -1,5 +1,8 @@
 package Try;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -23,16 +26,41 @@ public class SaveReadNote {
 
 			case 1:
 				try {
-					FileWriter fw = new FileWriter("javaStudies.txt");
+					FileWriter fw = new FileWriter("javaStudies.txt", true);
+					System.out.println("Enter your note:");
 					String writeSomething = scanner.nextLine();
-					
-					fw.write(writeSomething);
-					
+
+					fw.write(writeSomething + "\n");
+
+					fw.close();
 				} catch (IOException e) {
-					
+
 					e.printStackTrace();
 				}
+				break;
+			case 2:
+				BufferedReader br;
+				try {
+					br = new BufferedReader(new FileReader("javaStudies.txt"));
+					String line;
+					try {
+						while ((line = br.readLine()) != null) {
+							System.out.println(line);
+						}
+						br.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (FileNotFoundException e) {
+					System.out.println("No notes found yet");
+				}
+				break;
+			case 3:
+				System.out.println("System closing....");
+				break;
 				
+
 			}
 		}
 	}
