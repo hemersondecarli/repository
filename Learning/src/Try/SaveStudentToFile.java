604251a96ab082fefd6eaaf8e1aba2da3f172208
Hemerson <hemersondc96@gmail.com>
2026-06-04 23:14:38 +0100

    student class

---
+++ Learning/src/Try/SaveStudentToFile.java
@@ -0,0 +1,22 @@
+package Try;
+
+class Student {
+	private String name;
+	private int age;
+	private int grade;
+
+	public Student(String name, int age, int grade) {
+		this.name = name;
+		this.age = age;
+		this.grade = grade;
+	}
+}
+
+public class SaveStudentToFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
