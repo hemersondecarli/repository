792b20d5d961a10c11a0dccbffcf2f169dde1eac
Hemerson <hemersondc96@gmail.com>
2026-06-14 23:51:37 +0100

    Highest number

--- Learning/src/Learning/LargestNumber.java
+++ Learning/src/Learning/LargestNumber.java
@@ -3,20 +3,20 @@ package Learning;
 public class LargestNumber {
 
 	public static void main(String[] args) {
-		int [] numbers = {5, 12, 3, 20, 8};
+		int[] numbers = { 5, 12, 3, 20, 8 };
 
 		int currentNumber = 0;
-	
-		for (int i = 0; i<numbers.length; i++) {
-		
-			if(currentNumber < numbers[i]) {
-				 currentNumber = numbers[i];
-				
-			}else {
-				
+
+		for (int i = 0; i < numbers.length; i++) {
+
+			if (currentNumber < numbers[i]) {
+				currentNumber = numbers[i];
+
+			} else {
+
 			}
 		}
-		System.out.println(currentNumber);
+		System.out.println("Highest number is: " + currentNumber);
 	}
 
 }
